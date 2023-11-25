53527b2d3808dd1236faf5528783c93bf732a026
ZeroAddict <yemifrancisolayemi@gmail.com>
2023-11-24 18:02:22 -0800

    worked on the bash scripts, had issues come up in manage_my_proc and 7-highlander...102-zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,41 @@
+#include "pid_zombie.h"
+/**
+* infinite_while - opens an infinite while loop
+* Return: void
+*/
+
+int infinite_while(void)
+{
+	while (1)
+		sleep(1);
+	return (0);
+}
+
+
+/**
+* main - displays zombie proc x5, by callin fork x5 if pid > 0
+* Return: void
+*/
+int main(void)
+{
+	unsigned char index;
+	pid_t pid_child;
+
+	index = 1;
+	pid_child = fork();
+
+	if (index <= 5)
+	{
+		if (pid_child > 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			sleep(1);
+		}
+		index++;
+	}
+	else
+		exit(0);
+
+	infinite_while();
+	return (EXIT_SUCCESS);
+}
---
+++ 0x05-processes_and_signals/pid_zombie.h
@@ -0,0 +1,11 @@
+#ifndef PID_ZOMBIE_H
+#define PID_ZOMBIE_H
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+#endif
