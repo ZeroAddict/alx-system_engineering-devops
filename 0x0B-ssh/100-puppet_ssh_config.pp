a1999b37e909f623b9cfb6c512362bce53e8b956
ZeroAddict <yemifrancisolayemi@gmail.com>
2024-05-06 20:25:12 -0700

    Puppet manifest adds identityfile by path in root and changes PasswordAuthentication to no

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Using Puppet manifest to make changes to config file (smartly)
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content => ''
+
+	#SSH client cconfiguration adjusted and set
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
